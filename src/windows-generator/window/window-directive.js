89cd9a169f72164a7afd68b74c4c560c867cf1f4
mstaicu <md.staicu@yahoo.com>
2016-03-19 22:41:51 +0200

    Small comment indentation fix

--- src/windows-generator/window/window-directive.js
+++ src/windows-generator/window/window-directive.js
@@ -32,7 +32,7 @@ angular.module('window').directive('window', function() {
       /**
        * [getPeriodPercentage Gets a percentage based representation of a given period range]
        *
-			 The years are extracted from the dates that are passed as arguments
+       The years are extracted from the dates that are passed as arguments
        and are set as outer margins for the computed range
 
        The dates represent the inner range, inside the year range
